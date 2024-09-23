9509cf55e315e2e8a43692f2c37280d36b1ca7d8
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-09-23 12:09:41 -0600

    functions

---
+++ 05_functions.dart
@@ -0,0 +1,15 @@
+void main() {
+  print(greetEveryone());
+
+  print('Suma: ${addTwoNumbers(10, 20)}');
+  print('Suma: ${addTwoNumbersOptional(10)}');
+}
+
+String greetEveryone() => 'Hello everyone';
+
+int addTwoNumbers(int a, int b) => a + b;
+
+int addTwoNumbersOptional(int a, [int b = 0]) {
+  // b ??= 0;
+  return a + b;
+}
