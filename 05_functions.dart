5ebcd588477c37e546555e753bb0a0ee3238c1f7
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-09-23 14:38:36 -0600

    Parameters by name

--- 05_functions.dart
+++ 05_functions.dart
@@ -3,6 +3,8 @@ void main() {
 
   print('Suma: ${addTwoNumbers(10, 20)}');
   print('Suma: ${addTwoNumbersOptional(10)}');
+
+  print(greetPerson(name: 'Jonathan', message: 'Hi there!'));
 }
 
 String greetEveryone() => 'Hello everyone';
@@ -12,4 +14,9 @@ int addTwoNumbers(int a, int b) => a + b;
 int addTwoNumbersOptional(int a, [int b = 0]) {
   // b ??= 0;
   return a + b;
+}
+
+//Optional parameters by name
+String greetPerson({required name, String message = 'Hello,'}) {
+  return '$message $name';
 }
