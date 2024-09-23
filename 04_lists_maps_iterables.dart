c319156fd2b6c2b068743956b832d4bc313e9384
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-09-23 11:38:27 -0600

    Misc list operators

--- 04_lists_maps_iterables.dart
+++ 04_lists_maps_iterables.dart
@@ -12,10 +12,25 @@ void main() {
   print("List: ${reversedNumbers.toList()}");
   print("Set: ${reversedNumbers.toSet()}");
 
-  final numbersGreaterThan5 = numbers.where( (num) {
+  final numbersGreaterThan5 = numbers.where((num) {
     return num > 5;
   });
 
   print('>5 iterable: $numbersGreaterThan5');
   print('>5 to set: ${numbersGreaterThan5.toSet()}');
+
+//Spread operator
+  final List<int>? list_1 = [1, 2, 3];
+  final list_2 = [0, ...?list_1]; // Null-aware operator
+  print(list_2);
+
+// if statement inside list
+  bool promoActive = false;
+  List<String> nav = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
+  print(nav);
+
+// for statement inside list
+  List listOfInts = <int>[1, 2, 3];
+  List listOfStrings = <String>['#0', for (int i in listOfInts) '#$i'];
+  print(listOfStrings);
 }
