3a79f5a52fafd65cb1c4106d758841fb3ee52b8e
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-09-23 08:57:42 -0600

    first commit

---
+++ 01_hello_world.dart
@@ -0,0 +1,10 @@
+void main() {
+  // var myName = 'Jonathan';
+  // late final myName;
+  // String myName = 'Jonathan';
+  const myName = 'Jonathan';
+
+  print('Hello $myName');
+  print('Hello ${ myName.toUpperCase() }');
+  print('Hello ${ 1 + 1 }');
+}
