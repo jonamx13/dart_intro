c2e08167994091d39ab027b6e682c9aad0f4221d
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-09-26 14:26:16 -0600

    Extends

--- 09_abstract_classes.dart
+++ 09_abstract_classes.dart
@@ -1,15 +1,34 @@
-void main() {}
+void main() {
+  final windPlant = WindPlant(initiaEnergy: 100);
+
+  print('Wind: ${ chargePhone(windPlant)}');
+}
+
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception('Not enough energy');
+  }
+
+  return plant.energyLeft - 10;
+}
 
 enum PlantType { nuclear, wind, water }
 
 abstract class EnergyPlant {
   double energyLeft;
-  String type; // nuclear, wind, water
+  PlantType type; // nuclear, wind, water
 
-  EnergyPlant({
-    required this.energyLeft,
-    required this.type
-    });
-  
+  EnergyPlant({required this.energyLeft, required this.type});
+
   void consumeEnergy(double amount);
+}
+
+class WindPlant extends EnergyPlant {
+  WindPlant({required double initiaEnergy})
+      : super(energyLeft: initiaEnergy, type: PlantType.wind);
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
 }
